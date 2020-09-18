ec9107afdd4a7cb38e31cfb3dd3fa627b7506eba
Bartosz Wasilew <barofficial@vp.pl>
2020-09-18 19:48:26 +0200

    Register component implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./pokespring/home/home.component";
 import { LoginComponent } from './pokespring/login/login.component';
 import {PokedexComponent} from "./pokespring/pokedex/pokedex.component";
+import { RegisterComponent } from './pokespring/register/register.component';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'pokedex', component: PokedexComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
   {path: '**', redirectTo: '/home'}
 ];
 
